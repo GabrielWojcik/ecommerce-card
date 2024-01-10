4fa4465846610ea6edd72a116449a6115c4c84e8
Gabriel Wojcik <gabrielwojcikv@gmail.com>
2024-01-09 21:00:28 -0300

    Feat - adicionado a logica para curtir os produtos

--- src/components/card-itens/index.jsx
+++ src/components/card-itens/index.jsx
@@ -1,14 +1,24 @@
 import Image from "next/image"
-import img1 from "../../assets/banner-black.jpg"
 import { CardItensContainer, ContainerProduct, Star,
 Product, ProductDesc, ContainerFavorito, ContainerBtn} from "./styles"
-import { MdFavoriteBorder } from 'react-icons/md'
-import { PRODUCTS } from "@/src/data/products"
-import ButtonComponent from "../button-component/buttom"
-// import { NavLink, useParams } from 'react-router-dom';
-
+import { MdFavoriteBorder, MdOutlineFavorite} from 'react-icons/md'
+import { PRODUCTS } from "../../data/products"
+import { useState } from "react"
 
 export default function CardItens(){
+    const [likeProduct, setLikeProduct] = useState(false);
+    const [productLikeClick, setProductLikeClick] = useState('');
+
+    function handleClick(idItem) {
+        if (likeProduct && idItem === productLikeClick) {
+            setLikeProduct(false)
+            setProductLikeClick('')
+        } else {
+            setLikeProduct(true)
+            setProductLikeClick(idItem)
+        }
+    }
+
     return(
         <>
         {
@@ -16,11 +26,23 @@ export default function CardItens(){
                 return(
             <CardItensContainer key={value.id}>
                 <ContainerFavorito>
-                    <Star>
-                        <MdFavoriteBorder 
-                            size={18} 
-                            cursor="pointer"
-                         />
+                    <Star 
+                    onClick={() => handleClick(value.id) }>
+                        {
+                            likeProduct  && value.id === productLikeClick ? (
+                                <MdOutlineFavorite 
+                                    size={18}  
+                                    color="red" 
+                                    cursor="pointer"
+                                />
+                            )
+                            : (
+                            <MdFavoriteBorder 
+                                size={18} 
+                                cursor="pointer"
+                             />
+                            )
+                        }
                     </Star>
                 </ContainerFavorito>
             
@@ -45,7 +67,7 @@ export default function CardItens(){
                     <strong id="price">
                         R$ {value.price}
                     </strong>
-                    <p id="price-value">3x de R$ 22,97</p>
+                    <p data-testid="price" id="price-value">3x de R$ 22,97</p>
                     <p className="descricao">O Refil Creme Acetinado Desodorante Hidratante Corporal Lily 
                     entrega perfumação prolongada e fragrância intensa para sua pele na medida certa.</p>
                     <ContainerBtn>
