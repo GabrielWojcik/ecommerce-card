419f1c19151f67aa5adb7f00bac96a2b29f44947
GabrielWojcik <gabrielwojcikv@gmail.com>
2024-05-02 20:30:13 -0300

    build: adicionado validação com zod no .env

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-/** @type {import('next').NextConfig} */
+const envSchema = require('./schema.js');
+const { config } = require('dotenv');
+
+config();
+
+const { error, value: validatedEnv } = envSchema.parse(process.env);
+
+if (error) {
+  console.error('Erro ao validar variáveis de ambiente:', error);
+  process.exit(1); 
+}
+
 const nextConfig = {
+  env: validatedEnv,
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
